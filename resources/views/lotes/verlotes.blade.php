e485ca13e8ee33eb5ce691c4847ea7375bd7c117
theundertaker0 <amadeusdigital@gmail.com>
2020-10-02 16:11:36 -0500

    Añadido modal para los lotes

--- resources/views/lotes/verlotes.blade.php
+++ resources/views/lotes/verlotes.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
+
+<!-- Modal -->
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="tituloModal">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body" id='cuerpoModal'>
+          
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
     <div class="row my-4">
         <div class="col-lg-12 pt-2 text-center">
             <div class="pull-left">
@@ -14,7 +38,7 @@
             <img src="{{asset('img/mapa_fk.png')}}" name="kante" id="map-image" style="width: 1181px; max-width: 100%; height: auto;" alt="" usemap="#kante"/>
             <map name="kante" id="kante">
                 @foreach ($lotes as $lote )
-                    <area shape="poly" coords="{{$lote->coordenadas}}" href="#" title="Lote {{$lote->lote}}"/>
+                    <area shape="poly" coords="{{$lote->coordenadas}}" onclick="modal({{$lote}})" title="Lote {{$lote->lote}}"/>
                 @endforeach
             </map>
     </div>
@@ -23,10 +47,23 @@
 
 @section('js')
 <script src="{{asset('js/imageMapResizer.min.js')}}"></script>
+<script src="{{asset('js/jquery.numbers.min.js')}}"></script>
 <script type="text/javascript">
     $(document).ready(function(){
         $('map').imageMapResize();
     });
+
+
+    function modal(lote){
+        var cuerpo='<ul class="list-group">'+
+    '<li class="list-group-item">Frente: '+lote.frente+' Mts.  Fondo: '+lote.fondo+' Mts.  Área: '+lote.area+' M2</li>'+
+    '<li class="list-group-item">Precio M2: $'+$.number(lote.m2)+'  Precio Total: $'+$.number(lote.total)+'</li>'+
+    '<li class="list-group-item">Enganche: $'+lote.enganche+' Saldo: $'+lote.saldo+'</li>'+
+  '</ul>';
+        $('#tituloModal').text('Lote '+lote.lote);
+        $('#cuerpoModal').html(cuerpo);
+        $('#modal').modal('show');
+    }
 
 </script>
 @endsection
