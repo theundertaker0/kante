cb28fd99d8e72f0cdb4bbe3978fe2cb29a38247c
theundertaker0 <amadeusdigital@gmail.com>
2020-10-02 17:01:39 -0500

    corrección de protocolo

--- resources/views/lotes/verlotes.blade.php
+++ resources/views/lotes/verlotes.blade.php
@@ -46,8 +46,8 @@
 @endsection
 
 @section('js')
-<script src="{{asset('js/imageMapResizer.js')}}"></script>
-<script src="{{asset('js/jquery.numbers.min.js')}}"></script>
+<script src="{{asset('//js/imageMapResizer.js')}}"></script>
+<script src="{{asset('//js/jquery.numbers.min.js')}}"></script>
 <script type="text/javascript">
     $(document).ready(function(){
         $('map').imageMapResizeF();
